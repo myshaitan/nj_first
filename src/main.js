54a320bd091f2a55b39c60ed1b9e885c62fac7d6
myshaitan <myshaitan@gmail.com>
2022-06-11 16:50:12 +0900

    Setting prettier and eslint

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log('Hello world')
+console.log('Hello world')
+
+const x = 1;
+
+const http = require('http');
+const server = http.createServer('localhost:80');
+server.listen(l => l.console('hi'));
