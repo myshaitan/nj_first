0b2654618d12fbb0cb41f68da346412610b4d595
myshaitan <myshaitan@gmail.com>
2022-06-11 18:19:25 +0900

    htt listen test

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ console.log('Hello world')
 const x = 1;
 
 const http = require('http');
-const server = http.createServer('localhost:80');
-server.listen(l => l.console('hi'));
+const server = http.createServer();
+server.listen(1234, "localhost", () => {
+    console.log("aa");
+});
